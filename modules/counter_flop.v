52903ec33587dfe0e214af5e1d4f19fa0b9655b7
Evelyn Alexandra Nutt <evelynan@stanford.edu>
2023-12-07 17:16:19 -0800

    partially working counter_flop_tb

--- modules/counter_flop.v
+++ modules/counter_flop.v
@@ -1,31 +1,20 @@
 module counter_flop #(parameter X_BOX = 11'd0, Y_BOX = 10'd0, COUNT_SIZE = 5'd0) (
     input wire [COUNT_SIZE-1:0] count_enable_in,
     input wire clk, rst,
-    input wire [11:0] x,
-    input wire [10:0] y,
+    input wire [10:0] x,
+    input wire [9:0] y,
     output wire [3:0] count_enable_out,
-    output reg pixel_on
+    output wire pixel_on
 );
 
 wire [3:0] current_time, next_time, time_select;
-reg [19:0] prev_count;
+wire [19:0] prev_count;
+assign prev_count = (COUNT_SIZE == 5'd20) ? 20'd999999 : 20'd9;
 
-dffre #(3) counter(.clk(clk), .r(rst), .en(count_enable_in >= prev_count), .d(time_select), .q(current_time));
+dffre #(4) counter(.clk(clk), .r(rst), .en(count_enable_in >= prev_count), .d(time_select), .q(current_time));
 assign next_time = current_time + 4'd1;
 assign time_select = (next_time < 4'd9) ? next_time : 4'd0;
-assign count_enable_out = next_time;
-
-always @(*) begin
-    if (COUNT_SIZE == 5'd20) begin
-        assign prev_count = 20'd999999;
-    end
-    else if (COUNT_SIZE == 5'd4) begin
-        assign prev_count = 4'd9;
-    end
-    else begin
-        assign prev_count = 20'd0;
-    end
-end
+assign count_enable_out = current_time;
 
 char_display #(.X_BOX(X_BOX), .Y_BOX(Y_BOX)) digit_display(
     .pixel_x(x),
--- tests/counter_flop_tb.v
+++ tests/counter_flop_tb.v
@@ -5,10 +5,10 @@ module counter_flop_tb (
     reg [19:0] count_enable_in;
     reg clk;
     reg rst;
-    reg [11:0] x;
-    reg [10:0] y;
-    reg [3:0] count_enable_out;
-    reg [3:0] count_enable_out_2;
+    reg [10:0] x;
+    reg [9:0] y;
+    wire [3:0] count_enable_out;
+    wire [3:0] count_enable_out_2;
     wire pixel_on;
     wire pixel_on_2;
     reg [3:0] expected_count_ms;
@@ -17,6 +17,8 @@ module counter_flop_tb (
     parameter X_BOX = 11'd820;
     parameter Y_BOX = 10'd72;
     parameter COUNT_SIZE = 5'd20;
+    
+    reg [19:0] count;
 
     // Need to test 2 cases for when we need the ns count and when we don't
     
@@ -52,7 +54,7 @@ module counter_flop_tb (
         rst = 1'b1;
         x = 1'b0;
         y = 1'b0;
-        count_enable_in = 1'b0;
+        count_enable_in = 20'd0;
         expected_count_ms = 4'd0;
         expected_count_hs = 4'd0;
         #10;
@@ -60,54 +62,114 @@ module counter_flop_tb (
         
         /* For the ms clock, we should check that the count is going up every 999999ns or so
         Let's wait that long and check for a 1. */
-        #100000
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
+        
+        /* At this point, count_enable_in should equal 20'd999999, enabling the flip flop.
+        After the previous clock cycle, the flip flop should have incremented. */
         expected_count_ms = 4'd1;
         $display("actual %d, expected %d", count_enable_out, expected_count_ms);
+        count_enable_in = 20'd0;
 
-
-        /* On the waveform, count_enable_out should read 1. If so, we can assume that the char_display 
-        instantiation will work based on the complete functionality of our previous testbenches. 
-        For good measure, let's wait again and see the count go up. */
-        #100000
+        /* On the waveform, count_enable_out should read 1 like expected. If so, we can assume that 
+        the char_display instantiation will work based on the complete functionality of our previous
+        testbenches. For good measure, let's wait again and see the count go up. */
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
         expected_count_ms = 4'd2;
         $display("actual %d, expected %d", count_enable_out, expected_count_ms);
-        #100000
+        count_enable_in = 20'd0;
+
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
         expected_count_ms = 4'd3;
         $display("actual %d, expected %d", count_enable_out, expected_count_ms);
-        #100000
+        count_enable_in = 20'd0;
+        
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
         expected_count_ms = 4'd4;
         $display("actual %d, expected %d", count_enable_out, expected_count_ms);
+        count_enable_in = 20'd0;
 
         /* For the hs clock, we should check the the count is going up every 9ms, and count_enable_out
         for the ms clock should reset at that point. */
-        #100000
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
         expected_count_ms = 4'd5;
         $display("actual %d, expected %d", count_enable_out, expected_count_ms);
-        #100000
+        count_enable_in = 20'd0;
+
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
         expected_count_ms = 4'd6;
         $display("actual %d, expected %d", count_enable_out, expected_count_ms);
-        #100000
+        count_enable_in = 20'd0;
+        
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
         expected_count_ms = 4'd7;
         $display("actual %d, expected %d", count_enable_out, expected_count_ms);
-        #100000
+        count_enable_in = 20'd0;
+
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
         expected_count_ms = 4'd8;
         $display("actual %d, expected %d", count_enable_out, expected_count_ms);
-        #100000
-        expected_count_ms = 4'd9; // Our DFF waits a clock cycle before updating 
+        count_enable_in = 20'd0;
+        
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
+        expected_count_ms = 4'd9;
         $display("actual %d, expected %d", count_enable_out, expected_count_ms);
-        #100000
-
-        // It should have changed around here.
+        count_enable_in = 20'd0;
+        
+        // Wait another milisecond for the hs DFF to update
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
+        
+        // The hs DFF should have incremented here.
         expected_count_ms = 4'd0;
         expected_count_hs = 4'd1;
         $display("actual %d, expected %d", count_enable_out, expected_count_ms);
         $display("actual %d, expected %d", count_enable_out_2, expected_count_hs);
-
+        count_enable_in = 20'd0;
+        
         // Some more time for good measure. If nothing looks unordinary, this module works.
-        #100000
-        #100000
-        #100000
-        #100000
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
+        
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
+        
+        for (count = 20'd0; count < 20'd1000010; count = count + 1'd1) begin
+            count_enable_in = count;
+            #1;
+        end
 
         /* With this testbench, we will assume the complete functionality of the counter flop module
         implies the complete functionality of all instantiations of counter flop. */
